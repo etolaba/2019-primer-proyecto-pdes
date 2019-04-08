52c8771062130a8f199e8ff4b7aa9e620adaeb00
Esteban <egabrielt@gmail.com>
2019-04-07 23:06:34 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Golondrinas pueden comer y volar, cuando comen ganan 2 de energia por cada gramo que consuman
+// y cuando vuelan pierden 3 de energia por cada kilometro
+
+export function crearGolondrina() {
+     return { 
+        energia: 30,
+        volar: function(km){
+            this.energia -= km * 3
+        },
+        comer: function(gramos){
+            this.energia += gramos * 2
+        },
+     }
+ }
