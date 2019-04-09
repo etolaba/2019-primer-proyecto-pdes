2c56f5a72b0905467b4e9afd2886ba20e618177f
Esteban <egabrielt@gmail.com>
2019-04-08 21:42:42 -0300

    Cambiada la funcion constructora de golondrinas

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ export function crearGolondrina() {
         },
      }
  }
+
+export class Golondrina {
+    constructor(energia = 30){
+        this.energia = energia
+    }
+
+    volar(km){
+        this.energia -= km *3
+    }
+
+    comer(gramos){
+        this.energia += gramos * 2
+    }
+}
+
--- index.spec.js
+++ index.spec.js
@@ -1,14 +1,14 @@
-import { crearGolondrina } from './index.js'
+import { Golondrina } from './index.js'
 
 describe("pepita basica", () => {
     it("deberia volar y consumir energia", () => {
-        const pepita = crearGolondrina();
+        const pepita = new Golondrina(50);
         pepita.volar(2);
-        expect(pepita.energia).toEqual(24)
+        expect(pepita.energia).toEqual(44)
     })
 
     it("deberia comer y ganar energia", () => {
-        const pepita = crearGolondrina();
+        const pepita = new Golondrina();
         pepita.comer(3)
         expect(pepita.energia).toEqual(36)
     })
