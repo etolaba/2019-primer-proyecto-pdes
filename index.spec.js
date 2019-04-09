6a586e4c42d15aa373d9ddb4d0faa2f20dd8754c
Esteban <egabrielt@gmail.com>
2019-04-08 22:14:40 -0300

    agregar golondrinas inmutables

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-import { Golondrina } from './index.js'
+import { Golondrina, GolondrinasInmutables as GI} from './index.js'
 
 describe("pepita basica", () => {
     it("deberia volar y consumir energia", () => {
@@ -11,5 +11,12 @@ describe("pepita basica", () => {
         const pepita = new Golondrina();
         pepita.comer(3)
         expect(pepita.energia).toEqual(36)
+    })
+})
+
+describe("pepita inmutable", () => {
+    it("deberia volar y consumir energia", () => {
+        const pepita = GI.volar(GI.crear("pepita"), 2)
+        expect(pepita).toMatchObject({nombre: "pepita", edad: 1, energia: 24})
     })
 })
